b44c3cfbac126ff52168a4175654a59e814a1827
kaushaleshshukla <malviyankaushalesh@gmail.com>
2020-03-18 11:23:55 +0530

    cors origin update

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('views', path.join(__dirname + '/src/views'));
 app.set('view engine', 'ejs');
 
 let corsOptions = {
-	origin : 'http://localhost:3000',
+	origin : ['http://localhost:3000', 'http://localhost:3001', 'http://40.121.182.221:9000'],
 	credentials : true
 };
 app.use(cors(corsOptions));
